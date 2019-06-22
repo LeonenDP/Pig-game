9849d291189f3523e11e44e8fa416739bca89df2
Leon <tamazgan@gmail.com>
2019-06-22 14:16:56 +0300

    a

--- docs/app.js
+++ docs/app.js
@@ -78,7 +78,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
         // If the dice is 1
         else if(dice1 === 1 || dice2 === 1) {
-            alert('1 resets your level score');
+            document.querySelector('.game-info').textContent = '1 resets this round score.';
             setTimeout(nextPlayer, 1000);
         }
 
